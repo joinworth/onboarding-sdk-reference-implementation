dd31af1778369bb71812405c19c8d79f85f0a059
carlosrivera-ai <carlos.rivera@joinworth.com>
2026-01-02 09:02:47 -0500

    Change worth typo

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -11,7 +11,7 @@ import {
   customCss,
 } from '@/components/onboarding/constants';
 import { useNavigate } from 'react-router';
-import { useWorthContext } from '@/components/Worth/useWorthContext';
+import { useWorthContext } from '@/components/Worthx/useWorthContext';
 import { ORIGIN } from '@/constants/urls';
 
 const Onboarding = () => {
