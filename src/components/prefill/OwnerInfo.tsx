40ad3feb4821fe8ec597938edd905bcb82da01e8
Diego Pinzón <diego.pinzon@joinworth.com>
2026-01-09 09:12:14 -0500

    DOS-1041: Update sdk reference implementation to enable the applicant webapp to handle ownership (#18)
    * Update sdk reference implementation to enable the applicant webapp to handle ownership

--- src/components/prefill/OwnerInfo.tsx
+++ src/components/prefill/OwnerInfo.tsx
@@ -17,14 +17,12 @@ const OwnerInfo = ({ formData, onChange }: OwnerInfoProps): ReactElement => (
       name="owner1_first_name"
       value={formData.owner1_first_name}
       onChange={onChange}
-      required
     />
     <FormField
       label="Last Name"
       name="owner1_last_name"
       value={formData.owner1_last_name}
       onChange={onChange}
-      required
     />
     <FormField
       label="Email"
@@ -32,7 +30,6 @@ const OwnerInfo = ({ formData, onChange }: OwnerInfoProps): ReactElement => (
       type="email"
       value={formData.owner1_email}
       onChange={onChange}
-      required
     />
     <FormField
       label="Mobile"
@@ -40,14 +37,12 @@ const OwnerInfo = ({ formData, onChange }: OwnerInfoProps): ReactElement => (
       type="tel"
       value={formData.owner1_mobile}
       onChange={onChange}
-      required
     />
     <FormField
       label="SSN"
       name="owner1_ssn"
       value={formData.owner1_ssn}
       onChange={onChange}
-      required
     />
     <FormField
       label="Date of Birth"
@@ -55,35 +50,30 @@ const OwnerInfo = ({ formData, onChange }: OwnerInfoProps): ReactElement => (
       type="date"
       value={formData.owner1_dob}
       onChange={onChange}
-      required
     />
     <FormField
       label="Address Line 1"
       name="owner1_address_line_1"
       value={formData.owner1_address_line_1}
       onChange={onChange}
-      required
     />
     <FormField
       label="City"
       name="owner1_address_city"
       value={formData.owner1_address_city}
       onChange={onChange}
-      required
     />
     <FormField
       label="State"
       name="owner1_address_state"
       value={formData.owner1_address_state}
       onChange={onChange}
-      required
     />
     <FormField
       label="Postal Code"
       name="owner1_address_postal"
       value={formData.owner1_address_postal}
       onChange={onChange}
-      required
     />
     <FormField
       label="Country"
@@ -92,14 +82,12 @@ const OwnerInfo = ({ formData, onChange }: OwnerInfoProps): ReactElement => (
       value={formData.owner1_address_country}
       onChange={onChange}
       options={countryOptions}
-      required
     />
     <FormField
       label="Title"
       name="owner1_title"
       value={formData.owner1_title}
       onChange={onChange}
-      required
     />
     <FormField
       label="Owner Type"
@@ -108,7 +96,6 @@ const OwnerInfo = ({ formData, onChange }: OwnerInfoProps): ReactElement => (
       value={formData.owner1_owner_type}
       onChange={onChange}
       options={ownerTypeOptions}
-      required
     />
     <FormField
       label="Ownership Percentage"
@@ -116,7 +103,6 @@ const OwnerInfo = ({ formData, onChange }: OwnerInfoProps): ReactElement => (
       type="number"
       value={formData.owner1_ownership_percentage}
       onChange={onChange}
-      required
     />
   </>
 );
