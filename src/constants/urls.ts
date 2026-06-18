afbf90724617024c4243a050eb3acc0f88217d37
jessicachinn <chinn.jessica@gmail.com>
2026-06-18 12:27:52 -0400

    test default url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,4 @@
 export const API_URL =
-  import.meta.env.VITE_API_URL || 'https://api-dev.joinworth.ai';
+  import.meta.env.VITE_API_URL || 'https://api.staging.joinworth.com';
 export const ORIGIN =
   import.meta.env.VITE_ORIGIN || 'https://app.dev.joinworth.com';
